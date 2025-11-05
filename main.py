6e58e58da98d9dd84cfdf6051bb7b33bf0e8f64d
JROY18 <jinuroy2002@gmail.com>
2025-11-06 00:11:45 +0530

    Basic Application done

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ from PyQt5.QtCore import Qt
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()                                                                          #super() calls the parent class constructor
+        self.city_label = QLabel("Enter city name :",self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather",self)
+        self.teamperature = QLabel("70F",self)
+        self.emoji = QLabel("star",self)
+        self.description = QLabel("Sunny",self)
 
 if __name__ == "__main__":
-
         app = QApplication(sys.argv)
         weather_app = WeatherApp()
         weather_app.show()
-        sys.exit(app.exec_())                                                                      # Keeps your app running
+        sys.exit(app.exec_())                                                                      # Keeps your app running in a loop until an action is performed for returning exit code
